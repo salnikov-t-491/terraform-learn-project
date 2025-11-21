5de3db80f84b237b7efce52949db5893c397c231
Tim-Sa <salnikovtimofeyof@gmail.com>
2025-11-21 10:31:34 +0300

    deploy static

--- main.tf
+++ main.tf
@@ -1,10 +1,29 @@
+variable "domain" {
+  type = string
+}
+
+variable "folder_id" {
+  type = string
+}
+
+locals {
+  dns_zones = [yandex_dns_zone.zone1.id, yandex_dns_zone.zone2.id]
+}
+
+
 terraform {
+
+  # коннектор к YaCloud API
+
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
     }
   }
   required_version = ">= 0.13"
+
+
+  # state файл проекта в отдельном приватном бакете
 
   backend "s3" {
     endpoints = {
@@ -13,10 +32,32 @@ terraform {
     bucket = "terraform-project"
     region = "ru-central1"
     key    = "week1.tfstate"
+
+    # костыль для работающего подключения
     skip_region_validation = true
     skip_credentials_validation = true
     skip_requesting_account_id = true
     skip_s3_checksum = true
+  }
+  
+
+  # публичный бакет для шейринга статики
+
+  resource "yandex_storage_bucket" "open-bucket" {
+    bucket    = var.domain
+    folder_id = var.folder_id
+
+    # публичный доступ на чтение статики
+    anonymous_access_flags {
+        read        = true
+        list        = true
+        config_read = false
+    }
+
+    # страничка, опубликованная в бакете
+    website {
+        index_document = "index.html"
+    }
   }
 }
 
---
+++ variables.tfvars
@@ -0,0 +1,2 @@
+folder_id = "b1g8ig4qtvigbr0bqknu"
+domain = "terraform-project.ru"
